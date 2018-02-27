6a5db692f13ee306ce663822890d48f349e9ce51
mcllsti <darylmcallister@hotmail.co.uk>
2018-02-27 00:58:58 +0000

    Large amount of refactoring. Developed API warpper class and BaseController to help scale development

---
+++ IP3Project/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IP3Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace IP3Project.Controllers
+{
+    /// <summary>
+    /// Abstract base controller that all controllers will inherit from
+    /// </summary>
+    public abstract class BaseController : Controller
+    {
+        public IdeagenAPI API = new IdeagenAPI(); //initilize global API wrapper 
+
+    }
+}
---
+++ IP3Project/Models/IdeagenAPI.cs
@@ -0,0 +1,57 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IP3Project.Models
+{
+    /// <summary>
+    /// Wrapper Class that bridges the IdeagenAPI and RestSharp togither for ease of use and scaleability
+    /// Main purpose for encapsulation
+    /// </summary>
+    public class IdeagenAPI
+    {
+
+        const string BaseUrl = "https://gcu.ideagen-development.com";
+        private string AuthToken = "3dbd6fb2-caa0-4083-b286-6544baf2a248";
+        private string TenantId = "Team16";
+    
+        /// <summary>
+        /// Sets up a basic client object with the base url
+        /// </summary>
+        /// <param name="client">RestClient Object to setup with BaseURL</param>
+        /// <returns>RestClient object with url set </returns>
+        private RestClient SetupClientURL(RestClient client)
+        {
+            client.BaseUrl = new System.Uri(BaseUrl);
+            return client;
+        }
+
+        /// <summary>
+        /// Executes a Get Request to the API
+        /// </summary>
+        /// <param name="request">RestRequest Object that has had endpoint set</param>
+        /// <returns></returns>
+        public IRestResponse GetRequest(RestRequest request)
+        {
+            //sets up the client and the base URL
+            var client = new RestClient();
+            client = SetupClientURL(client);
+
+            //Set appropriate request properties
+            request.Method = Method.GET;
+            request.AddHeader("AuthToken", AuthToken);
+            request.AddHeader("TenantId", TenantId);
+
+
+            IRestResponse response = client.Execute(request); //execures request 
+
+            return response;
+
+        }
+        
+
+    }
+}
