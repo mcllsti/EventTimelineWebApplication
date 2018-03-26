b51e73e0c554fc6f2f8368432e5560e06a76acb8
mcllsti <darylmcallister@hotmail.co.uk>
2018-03-26 17:17:04 +0100

    Events will initally display. Code Refactor is absolute must

---
+++ IP3Project/Controllers/EventController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IP3Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace IP3Project.Controllers
+{
+    public class EventController : BaseController
+    {
+        public IActionResult EventRegister(string Id )
+        {
+
+            List<TimelineEventViewModel> list = new List<TimelineEventViewModel>();
+            list = GetAllTimelines(list,Id);
+
+            List<TimelineEventsViewModel> model = new List<TimelineEventsViewModel>();
+            model = list.First().TimelineEvents;
+
+
+
+
+
+            return PartialView("_EventRegister", model);
+
+        }
+
+        private List<TimelineEventViewModel> GetAllTimelines(List<TimelineEventViewModel> model, string Id)
+        {
+
+            var request = new RestRequest("Timeline/GetAllTimelinesAndEvent"); //setting up the request params
+            IRestResponse response = API.GetRequest(request); //Uses IdeagenAPI wrapperclass to make a request and retreives the response
+
+            var resultsDTO = JsonConvert.DeserializeObject<Timelinez>(response.Content); //Deserializes the results from the response
+
+            foreach(TimelineEventViewModel x in resultsDTO.Timelines.Where(x => x.Id.Equals(Id)))
+            {
+                model.Add(x);
+            }
+
+            return model;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
--- IP3Project/Models/EventViewModel.cs
+++ IP3Project/Models/EventViewModel.cs
@@ -4,15 +4,65 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace IP3Project.Models
-{
-    public class Event
-    {
-        public string EventId { get; set; }
-        public string EventTitle { get; set; }
-        public string EventDescription { get; set; }
-        public string EventDateTime { get; set; }
-        public string EventLocation { get; set; }
 
+
+{
+
+    public class Timelinez
+    {
+        public List<TimelineEventViewModel> Timelines { get; set; }
+
+        public Timelinez()
+        {
+            Timelines = new List<TimelineEventViewModel>();
+        }
+
+    }
+
+
+    public class TimelineEventViewModel
+    {
+        public string Title { get; set; }
+
+        public string CreationTimeStamp { get; set; }
+        public string Id { get; set; }
+        public List<TimelineEventsViewModel> TimelineEvents { get; set; }
+
+        public TimelineEventViewModel()
+        {
+            TimelineEvents= new List<TimelineEventsViewModel>();
+        }
+
+    }
+
+
+    public class TimelineEventsViewModel
+    {
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string tDateTime { get; set; }
+        public string Location { get; set; }
+        public List<tempAttachment> Attachments { get; set; }
+
+        public TimelineEventsViewModel()
+        {
+            Attachments = new List<tempAttachment>();
+        }
+
+        //LINKEDEVENTS?
+        //ATTACHEMENTS?
+
+    }
+
+    public class tempAttachment
+    {
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string TimelineEventId { get; set; }
+        
     }
 
 
