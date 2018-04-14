f413bc5fefaf284ba227889c4e78e4c5bd013a8b
Gavin <gamac97@hotmail.co.uk>
2018-04-14 18:16:33 +0100

    Style fixes, attachment delete button

---
+++ IP3Project/obj/Release/netcoreapp2.0/PubTmp/Out/wwwroot/js/validationReset.js
@@ -0,0 +1,17 @@
+﻿/*
+
+Author: Team16
+Date: Trimester 2, 2018
+Version: 1.0
+
+Used to ensure that validation is hitting forms in cascading partial views
+
+
+*/
+
+///Resets form validation
+$(function () {
+    var form = $("#Form");
+    $(form).removeData("validator");
+    $.validator.unobtrusive.parse(form);
+});
